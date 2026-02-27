2a6a024c7f0066f0fa5107b3497d580eb9ba6363
FranDefense <fran.maestre.munoz@gmail.com>
2026-02-27 11:17:26 +0100

    Lanzamiento v1.5

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'layer4-v1.6'; // Cambia esto para forzar actualización
+const assets = [
+  'index.html',
+  'consumos_v3.html',
+  'vozydatos_exp.html',
+  'calculadoras.html',
+  'changelog.html',
+  'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js'
+];
+
+// Instalación: Guarda los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+// Estrategia: Carga desde caché, pero busca en la red si hay cambios
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
