8d2f710c73a4ede7ff81a844eda9fd55c3a602fa
FranMaestre <fran.maestre.munoz@gmail.com>
2026-02-27 14:50:32 +0100

    Update service worker cache version to v1.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'layer4-v1.8'; // Cambia esto para forzar actualización
+const CACHE_NAME = 'layer4-v1.9'; // Cambia esto para forzar actualización
 const assets = [
   'index.html',
   'consumos_v3.html',
@@ -39,6 +39,7 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
 
 
 
